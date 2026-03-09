ad8ae5d303ca7e834a01a36aae2b19295c8cccce
Danilo <danilosoaresmatos@gmail.com>
2026-03-09 14:28:39 -0300

    Testes e conclusão do projeto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# linked_list
+# Listas Linkadas
+
+linked_list.py está as classes do Nó e da Lista, já no main estão os testes.
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 ﻿from linked_list import ListaLigada
 
 def main():
-    pass
+    minha_lista = ListaLigada()
+
+    print("--- Inserindo Elementos ---")
+    minha_lista.insert_start(10)
+    minha_lista.insert_end(20)
+    minha_lista.insert_end(30)
+    minha_lista.insert_start(5)
+    minha_lista.insert_end(40)
+
+    print("Estado atual da lista:")
+    minha_lista.display()
+
+    print("\nRemovendo o valor 20")
+    minha_lista.remove(20)
+    minha_lista.display()
+
+    print("\nBuscando valor")
+    print(minha_lista.search(30))
+
+    print(minha_lista.search(100))
 
 main()
